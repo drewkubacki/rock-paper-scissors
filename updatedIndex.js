4b2c03b336dbf5a4b7d99f67fdb635dff6da9ce4
drewkubacki <111913572+drewkubacki@users.noreply.github.com>
2023-01-23 14:47:33 -0500

    Completed game functionality which allows RPS to be played to a score of 5 with proper handling of the selection and reset buttons

--- updatedIndex.js
+++ updatedIndex.js
@@ -1,3 +1,6 @@
+//RPS === Rock, Paper, Scissors 
+
+//Selects all the buttons on the page which pertain to RPS choices and adds a 'click' event listener
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -11,6 +14,17 @@ buttons.forEach((button) => {
     });
 });
 
+//Disables the RPS selection buttons
+function disableButtons() {
+    buttons.forEach((button) => button.disabled = true);
+};
+
+//Enables the RPS selection buttons
+function enableButtons() {
+    buttons.forEach((button) => button.disabled = false);
+};
+
+//Uses Math.random to get a random choice from the choices array to be used as the computer selection
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     let randomIndex = Math.floor(Math.random() * choices.length);
@@ -18,37 +32,64 @@ function getComputerChoice() {
     return computerSelection;
 };
 
+//This function creates the reset button which is populated when a player reaches a score of 5
+function createResetButton() {
+    let resetButton = document.createElement("button");
+    resetButton.textContent = "Reset";
+    document.body.appendChild(resetButton);
+    resetButton.onclick = function(){
+        let playerOneScore = 0;
+        let playerTwoScore = 0;
+        document.querySelector('#playerOneScore').textContent = playerOneScore;
+        document.querySelector('#playerTwoScore').textContent = playerTwoScore;
+        enableButtons();
+        document.body.removeChild(resetButton);
+    }
+};
+
+//This function is called when a RPS button is selected, the button.id and the return value from getComputerChoice are used to play
 function playRound(playerSelection, computerSelection) {
+    //Parses the document to grab both player scores which have a correlating ID in the HTML file
     let playerOneScore = Number(document.querySelector('#playerOneScore').textContent);
     let playerTwoScore = Number(document.querySelector('#playerTwoScore').textContent);
     let roundMessage = document.querySelector('#roundMessage');
     let message = "";
     computerSelection = getComputerChoice();
 
+    //The user choice (button.id) and computerSelection are equivalent this runs
     if (playerSelection === computerSelection) {
         message = "It's a tie!";
         roundMessage.textContent = message;
         return [playerSelection, message];
-
-     } else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock"){
+    
+    //The computerSelection beats the user choice (button.id), then this runs
+    } else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock"){
         message = `You Lose! ${computerSelection} beats ${playerSelection}`;
-        console.log(message);
         let score = playerTwoScore += 1;
-        document.querySelector('#playerTwoScore').textContent = score;
-        roundMessage.textContent = message;
+        if (score < 5) {
+            document.querySelector('#playerTwoScore').textContent = score;
+            roundMessage.textContent = message;
+            } else {
+                document.querySelector('#playerTwoScore').textContent = score;
+                roundMessage.textContent = "You lost to a Computer!";
+                disableButtons();
+                createResetButton();
+            }
         return [playerSelection, message];
-
-     } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"){
+    
+    //The user choice (button.id) beats the computerSelection, then this runs
+    } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"){
         message = `You Win! ${playerSelection} beats ${computerSelection}`;
-        console.log(message);
         let score = playerOneScore += 1;
+        if (score < 5) {
         document.querySelector('#playerOneScore').textContent = score;
         roundMessage.textContent = message;
-        return [playerSelection, message];
-
-     } else {
-        message = "Please enter one of the 3 choices...Bozo!";
-        console.log(message);
+        } else {
+            document.querySelector('#playerOneScore').textContent = score;
+            roundMessage.textContent = "You beat a Computer!";
+            disableButtons();
+            createResetButton();
+        }
         return [playerSelection, message];
      }
 };
